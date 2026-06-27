ccab89593109d9edfae9ada441550465a6557274
GorlikItsMe <GorlikItsMe@users.noreply.github.com>
2026-06-27 16:30:40 +0200

    Enhance stb_image_write implementation by adding diagnostic pragmas to suppress specific warnings for better compatibility with Clang and GCC.

--- source/stb_image_write_impl.cpp
+++ source/stb_image_write_impl.cpp
@@ -2,6 +2,15 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wconversion"
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#endif
 #include <onexexplorer/stb_image_write.h>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #pragma GCC diagnostic pop
 // clang-format on
