1372fb9e398eec009d8e6f254a60745bd7ab8bd1
GorlikItsMe <GorlikItsMe@users.noreply.github.com>
2026-06-27 16:26:44 +0200

    Add stb_image_write implementation and clean up onexplorer.cpp by removing redundant code.

---
+++ source/stb_image_write_impl.cpp
@@ -0,0 +1,7 @@
+// clang-format off
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#include <onexexplorer/stb_image_write.h>
+#pragma GCC diagnostic pop
+// clang-format on
--- test/source/onexplorer.cpp
+++ test/source/onexplorer.cpp
@@ -1,12 +1,5 @@
-// clang-format off
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#include <onexexplorer/stb_image_write.h>
-#pragma GCC diagnostic pop
-// clang-format on
-
 #include <doctest/doctest.h>
+#include <onexexplorer/stb_image_write.h>
 #include <onexexplorer/version.h>
 
 #include <string>
