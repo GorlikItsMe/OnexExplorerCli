ead771c3ec25a6ee22003b53dbee4ae7161d5fdd
GorlikItsMe <GorlikItsMe@users.noreply.github.com>
2026-06-28 15:40:20 +0200

    Remove unused Span class from error.h (#4)

--- include/onex/core/error.h
+++ include/onex/core/error.h
@@ -1,37 +1,10 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
 
 namespace onex {
 
   using byte = std::uint8_t;
-
-  template <typename T> class Span {
-  public:
-    Span() : data_(nullptr), size_(0) {}
-    Span(T* data, std::size_t size) : data_(data), size_(size) {}
-
-    auto data() const -> T* { return data_; }
-    auto size() const -> std::size_t { return size_; }
-    auto empty() const -> bool { return size_ == 0; }
-
-    auto begin() const -> T* { return data_; }
-    auto end() const -> T* { return data_ + size_; }
-
-    auto operator[](std::size_t i) const -> T& { return data_[i]; }
-
-    auto subspan(std::size_t offset, std::size_t count) const -> Span {
-      return Span(data_ + offset, count);
-    }
-
-  private:
-    T* data_;
-    std::size_t size_;
-  };
-
-  using ByteSpan = Span<const byte>;
-  using MutableByteSpan = Span<byte>;
 
   enum class Error {
     kNone,
